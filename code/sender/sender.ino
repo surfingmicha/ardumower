6f6403f94179680ec0bd53daa2738836136dacbb
surfingmicha <surfing.micha@freenet.de>
2018-05-01 20:58:00 +0200

    Update sender.ino

--- code/sender/sender.ino
+++ code/sender/sender.ino
@@ -34,9 +34,9 @@
 
 
 // ---- choose only one perimeter signal code ----
-#define SIGCODE_1  // Ardumower default perimeter signal
+//#define SIGCODE_1  // Ardumower default perimeter signal
 //#define SIGCODE_2  // Ardumower alternative perimeter signal
-//#define SIGCODE_3  // Ardumower alternative perimeter signal
+#define SIGCODE_3  // Ardumower alternative perimeter signal
 
 
 // --- MC33926 motor driver ---
